e1df20a5b8cbce6bd4e0ce70a80f74b13d9b8b22
GitMe23 <andyhop@hotmail.co.uk>
2023-12-28 18:26:17 +0000

    Refactored low cut filter configuration

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -111,6 +111,128 @@ private:
     using Coefficients = Filter::CoefficientsPtr;
     static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
     
+    template<typename ChainType, typename CoefficientType>
+    void updateCutFilter(ChainType& leftLowCut,
+                         const CoefficientType& cutCoefficients,
+                         const Slope& lowCutSlope)
+
+                        
+    {
+   
+        
+        // bypass all links in the chain:
+        leftLowCut.template setBypassed<0>(true);
+        leftLowCut.template setBypassed<1>(true);
+        leftLowCut.template setBypassed<2>(true);
+        leftLowCut.template setBypassed<3>(true);
+        
+        // We want to switch based on the slope setting. We've defined an enum to define slope setting in headers file
+        
+        /*
+         Recall that for slope, we used AudioParameterChoice for choice of 4 settings in db/oct: 12, 24, 36, 48 (not linear slider)
+
+         Check impl of helper method designIIRHighpassHighOrderButterworthMethod
+         We need to pass it an order corresponding below:
+              
+         Slope choice 0: 12 db/oct -> order: 2
+         Slope choice 1: 24 db/oct -> order: 4
+         Slope choice 2: 36 db/oct -> order: 6
+         Slope choice 3: 48 db/oct -> order: 8
+
+         */
+        switch( lowCutSlope )
+        {
+                
+            /*
+             if order is 2, meaning 12 db/oct slope, the helper method will return an array of 1 coefficient object only
+            which we will assign to the first filter in the cut filter chain. Also, stop bypassing that filter chain
+             */
+            case Slope_12:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                break;
+            }
+            case Slope_24:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                break;
+            }
+            case Slope_36:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+                leftLowCut.template setBypassed<2>(false);
+                break;
+            }
+            case Slope_48:
+            {
+                *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
+                leftLowCut.template setBypassed<0>(false);
+                *leftLowCut.template get<1>().coefficients = *cutCoefficients[1];
+                leftLowCut.template setBypassed<1>(false);
+                *leftLowCut.template get<2>().coefficients = *cutCoefficients[2];
+                leftLowCut.template setBypassed<2>(false);
+                *leftLowCut.template get<3>().coefficients = *cutCoefficients[3];
+                leftLowCut.template setBypassed<3>(false);
+                break;
+            }
+        }
+        
+        auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
+        
+        rightLowCut.setBypassed<0>(true);
+        rightLowCut.setBypassed<1>(true);
+        rightLowCut.setBypassed<2>(true);
+        rightLowCut.setBypassed<3>(true);
+            
+        switch ( lowCutSlope )
+        {
+
+            case Slope_12:
+            {
+                *rightLowCut.get<0>().coefficients = *cutCoefficients[0];
+                rightLowCut.setBypassed<0>(false);
+                break;
+            }
+            case Slope_24:
+            {
+                *rightLowCut.get<0>().coefficients = *cutCoefficients[0];
+                rightLowCut.setBypassed<0>(false);
+                *rightLowCut.get<1>().coefficients = *cutCoefficients[1];
+                rightLowCut.setBypassed<1>(false);
+                break;
+            }
+            case Slope_36:
+            {
+                *rightLowCut.get<0>().coefficients = *cutCoefficients[0];
+                rightLowCut.setBypassed<0>(false);
+                *rightLowCut.get<1>().coefficients = *cutCoefficients[1];
+                rightLowCut.setBypassed<1>(false);
+                *rightLowCut.get<2>().coefficients = *cutCoefficients[2];
+                rightLowCut.setBypassed<2>(false);
+                break;
+            }
+            case Slope_48:
+            {
+                *rightLowCut.get<0>().coefficients = *cutCoefficients[0];
+                rightLowCut.setBypassed<0>(false);
+                *rightLowCut.get<1>().coefficients = *cutCoefficients[1];
+                rightLowCut.setBypassed<1>(false);
+                *rightLowCut.get<2>().coefficients = *cutCoefficients[2];
+                rightLowCut.setBypassed<2>(false);
+                *rightLowCut.get<3>().coefficients = *cutCoefficients[3];
+                rightLowCut.setBypassed<3>(false);
+                break;
+            }
+        }
+    }
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
